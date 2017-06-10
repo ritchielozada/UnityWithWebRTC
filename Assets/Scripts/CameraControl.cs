85677d84d199b2f1c5c8fedf8c3469f553ab8a64
Ritchie Lozada <rlozada@outlook.com>
2017-06-09 18:58:10 -0700

    Initial Project Checkin

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// Ritchie Lozada (rlozada@microsoft.com)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float MoveRate = 1f;
+    public float TurnRate = 90f;
+   
+
+    void Update()
+    {
+        var pitch = Input.GetAxis("Vertical");
+        var yaw = Input.GetAxis("Horizontal");
+
+        var mx = Input.GetAxis("MoveX");
+        var my = Input.GetAxis("MoveY");
+        var mz = Input.GetAxis("MoveZ");
+
+        var MouseX = Input.GetAxis("Mouse X");
+        var MouseY = Input.GetAxis("Mouse Y");
+
+        // Right Mouse Button
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(-MouseY, MouseX, 0f) * Time.deltaTime * TurnRate);
+        }
+
+        // Left Mouse Button
+        if (Input.GetMouseButton(0))
+        {            
+            transform.Translate(new Vector3(MouseX, 0f, MouseY) * Time.deltaTime * MoveRate);
+        }
+
+        transform.Rotate(new Vector3(pitch, yaw, 0f) * Time.deltaTime * TurnRate);
+        transform.Translate(new Vector3(mx, my, mz) * Time.deltaTime * MoveRate);
+    }
+}
---
+++ Assets/Scripts/UIControls.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// Ritchie Lozada (rlozada@microsoft.com)
+
+
+using System.Collections;
+using UnityEngine;
+
+public class UIControls : MonoBehaviour
+{    
+    public RectTransform panelMain;
+
+    private IEnumerator panelCoRoutineControl;
+    private float width;
+    private float height;
+    private float xPos;
+    private float yPos;
+    private bool isPanelOpen;
+    private float moveSpeed;
+
+    void Start()
+    {
+        width = panelMain.rect.width;
+        height = panelMain.rect.height;
+        moveSpeed = height * 3f;
+
+        xPos = panelMain.rect.x;
+        isPanelOpen = true;
+        panelCoRoutineControl = panelCoRoutine(height, 1);
+    }
+
+    public void TogglePanel()
+    {        
+        if (isPanelOpen)
+        {
+            ClosePanel();
+        }
+        else
+        {
+            OpenPanel();
+        }
+        isPanelOpen = !isPanelOpen;
+    }
+
+
+    public void OpenPanel()
+    {
+        StopCoroutine(panelCoRoutineControl);
+        panelCoRoutineControl = panelCoRoutine(0, 1);
+        StartCoroutine(panelCoRoutineControl);
+    }
+
+    public void ClosePanel()
+    {
+        StopCoroutine(panelCoRoutineControl);
+        panelCoRoutineControl = panelCoRoutine(-height, -1);
+        StartCoroutine(panelCoRoutineControl);
+    }
+
+    IEnumerator panelCoRoutine(float yTargetValue, int direction)
+    {     
+        var targetY = yTargetValue;
+        yPos = panelMain.anchoredPosition.y;
+
+        while (
+                ((direction > 0) &&  (yPos < targetY)) || 
+                ((direction < 0) && (yPos > targetY))         
+              )
+        {
+            yPos += direction * moveSpeed * Time.deltaTime;
+            panelMain.anchoredPosition = new Vector3(xPos, yPos);
+            yield return null;
+        }
+        panelMain.anchoredPosition = new Vector3(xPos, yTargetValue);
+        yield return null;
+    }   
+}
